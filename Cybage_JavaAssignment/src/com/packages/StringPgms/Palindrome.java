6dbd43e8102fb385529d624b819354c3901d56d6
aadityn <aadityn@Aadityn-VD3.cybage.com>
2022-11-09 09:24:06 +0530

    new palindrome code added

--- Cybage_JavaAssignment/src/com/packages/StringPgms/Palindrome.java
+++ Cybage_JavaAssignment/src/com/packages/StringPgms/Palindrome.java
@@ -6,22 +6,26 @@ class Palindrome
 {
    public static void main(String args[])
    {
-      String Name, strrev = "";
-      Scanner sc = new Scanner(System.in);
- 
-      System.out.println("Enter a string:");
-      Name = sc.nextLine();
- 
-      int length = Name.length();
- 
-      for ( int i = length - 1; i >= 0; i-- )
-         strrev = strrev + Name.charAt(i);
- 
-      if (Name.equals(strrev))
-         System.out.println("The Entered String is a palindrome");
+      String strrev="";
+      
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Please Enter a string to Check whether its palindrome or Not");
+      String Name=sc.nextLine();
+      
+      int len= Name.length();
+      
+      for (int i=len-1;i>=0;i--)
+      {
+    	  strrev= strrev+Name.charAt(i);
+      }
+      
+      if(Name.equals(strrev))
+      {
+    	  System.out.println("The Entered String is a palindrome");
+      }
       else
-         System.out.println("The Entered String is not a palindrome");
- 
+          System.out.println("The Entered String is not a palindrome");
+      
       sc.close();
    }
 }
